212079a25ea38f8e9f840eeb24f681a44f893eb6
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-08-14 19:07:09 -0400

    Followed an article to add a custom hook and tried accepting a user's input to create a custom API request based on the date they wanted to see. Added a customHook.js file to handle that. It's not displaying just yet but MVP still works.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios';
 import CreateCard from './CreateCard';
+import DatePicker from './DatePicker';
 import { tsPropertySignature } from "@babel/types";
 
 function App() {
@@ -17,16 +18,18 @@ function App() {
     const apod = res.data;
     console.log("NASA apod data: ", apod);
     setApodData(apod);
+  })
+  .catch(err => {
+    console.log(err);
   });
-
-  //   .catch(err => {
-  //   console.log(err);
-  // });
   }, []);
 
 
   return (
     <div className="App">
+
+      <DatePicker />
+
       <img src={apodData.url} />
       <CreateCard title={apodData.title} image={apodData.url} description={apodData.explanation}/>
     </div>
